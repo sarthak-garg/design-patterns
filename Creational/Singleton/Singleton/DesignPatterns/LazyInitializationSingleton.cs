f7513070c194b2a763075000424e8d8056e68be1
Sarthak Garg <sarthak1.garg@gmail.com>
2018-01-15 22:20:59 +0530

    Added Singleton Design Pattern w/ implementation of LazyInitializationSingleton.

---
+++ Creational/Singleton/Singleton/DesignPatterns/LazyInitializationSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton.DesignPatterns
+{
+    public class LazyInitializationSingleton
+    {
+        ///<summary>
+        /// Private static instance of the same class.
+        ///</summary>
+        private static LazyInitializationSingleton instance;
+
+        ///<summary>
+        /// Private constructor.
+        ///</summary>
+        private LazyInitializationSingleton() { }
+
+        ///<summary>
+        /// Public static method to return an instance of the class.
+        /// If the instance is null at first, then initializes it.
+        ///</summary>
+        public static LazyInitializationSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new LazyInitializationSingleton();
+            }
+
+            return instance;
+        }
+
+        ///<summary>
+        /// Test the instance of the class.
+        ///</summary>
+        public void TestInstance()
+        {
+            Console.WriteLine("Instance Type: {0}.", instance.GetType());
+        }
+    }
+}
---
+++ Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using Singleton.DesignPatterns;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lazy Initialization Singleton Design Pattern
+            LazyInitializationSingleton lazyInitializationSingleton = LazyInitializationSingleton.GetInstance();
+            lazyInitializationSingleton.TestInstance();
+        }
+    }
+}
